9fe063aea27aece4520ce521c531b7866438c07f
D-a-k-a-n <190107008@stu.sdu.edu.kz>
2021-01-11 00:33:06 +0500

    done

--- loops-for/arrayLoop.py
+++ loops-for/arrayLoop.py
@@ -1,6 +1,6 @@
 def insert_squares(arr, num):
     # add square of numbers from 1 to num to the list named arr and return list
-    for i in range(1, num):
+    for i in range(1, num+1):
         arr.append(i**2)
 
     return arr
