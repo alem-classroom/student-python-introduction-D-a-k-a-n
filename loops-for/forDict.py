8156da92ec90bd006d119ceb1823d7a761f3b1da
D-a-k-a-n <190107008@stu.sdu.edu.kz>
2021-01-11 00:05:43 +0500

    done

--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -1,2 +1,8 @@
 def reverse_dict(dict):
     # swap keys and values within dict and return dict
+    x = dict.keys()
+    res = {}
+    for i in list(x).items():
+    	res[dict[i]] = i
+
+    return res
