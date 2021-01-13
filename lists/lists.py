5ddcaa88c8a8b3351e7f5d069311e0eece6c90ad
D-a-k-a-n <190107008@stu.sdu.edu.kz>
2021-01-13 18:53:06 +0500

    done

--- lists/lists.py
+++ lists/lists.py
@@ -15,7 +15,7 @@ def delete_elem_from_list(list, index = -1):
 
 
 def count_elements_in_list(list, x):
-    # count elements in the list that are equal to x and return the coun
+    # count elements in the list that are equal to x and return the count
     return list.count(x)
 
 
